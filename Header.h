ebcda4a7d3fdb5f094e0dfca01f69578b93d965c
codekavi <code.kavi@gmail.com>
2016-03-06 22:22:56 -0800

    how are you

---
+++ Header.h
@@ -0,0 +1,19 @@
+//
+//  Header.h
+//  iFlyChatGlobalListSwiftView
+//
+//  Created by iFlyLabs on 04/08/15.
+//  Copyright (c) 2015 iFlyLabs. All rights reserved.
+//
+
+#ifndef iFlyChatGlobalListSwiftView_Header_h
+#define iFlyChatGlobalListSwiftView_Header_h
+
+
+#endif
+
+
+#import "iFlyChatLibrary/iFlyChatLibrary.h"
+#import "ChatTableViewCell.h"
+#import "ChatCellSettings.h"
+#import "ContentView.h"
